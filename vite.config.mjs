6ba45f85698f2e0b0cf7c4ad4e1a912a23562d37
Nicholayton11 <nicholas.a.layton@gmail.com>
2024-07-15 22:33:27 +0100

    updated Node Modules and now running

--- vite.config.mjs
+++ vite.config.mjs
@@ -11,6 +11,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // base: "https://github.com/Nicholayton11/Iconick.git",
   plugins: [
     VueRouter(),
     Vue({
