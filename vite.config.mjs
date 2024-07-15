1b596dc212041cfe1d61c05d381c3aeb7959321c
Nicholayton11 <nicholas.a.layton@gmail.com>
2024-07-15 23:00:02 +0100

    added dist file

--- vite.config.mjs
+++ vite.config.mjs
@@ -11,7 +11,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "https://github.com/Nicholayton11/Iconick.git",
+  base: "https://github.com/Nicholayton11/Iconick.git",
   plugins: [
     VueRouter(),
     Vue({
